16543c1893edc2d5636257d1acdb68707e853b12
kavya <kavya.actimize@gmail.com>
2026-07-15 12:26:09 +0530

    add

--- src/API/API.js
+++ src/API/API.js
@@ -15,128 +15,90 @@ class API {
   }
 
   async get(url, data) {
-    return new Promise((resolve, reject) => {
-      console.log('this is getAPI');
-      this.api(METHOD.GET, url, data)
-        .then((response) => {
-          resolve(response);
-          // if(response.data){
-          //   toast.success(" get request successfully",response);
-
-          // }
-          // else{
-          //   toast.error("Get Request Failed",response);
-          // }
-          console.log("this is response2", response)
-        })
-        .catch((error) => {
-          toast.error("Something went wrong");
-          console.log(error);
-        });
-    });
-
+    try {
+      const response = await this.api(METHOD.GET, url, data);
+      return response;
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something went wrong");
+      throw error;
+    }
   }
 
-  post(url, data) {
-    return new Promise((resolve, reject) => {
-      console.log("this is postAPI");
+  async post(url, data) {
+    try {
+      const response = await this.api(METHOD.POST, url, data);
 
-      this.api(METHOD.POST, url, data)
-        .then((response) => {
-          toast.success(response?.data?.message);
+      toast.success(response?.data?.message || "Success");
 
-          resolve(response);
-        })
-        .catch((error) => {
-          toast.error("Something went wrong");
-
-          reject(error);
-        });
-    });
+      return response;
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something went wrong");
+      throw error;
+    }
   }
-  put(url, data) {
-    return new Promise((resolve, reject) => {
 
+  async put(url, data) {
+    try {
+      const response = await this.api(METHOD.PUT, url, data);
 
-      this.api(METHOD.PUT, url, data)
-        .then((response) => {
-          toast.success(response?.data?.message);
+      toast.success(response?.data?.message || "Updated Successfully");
 
-          resolve(response);
-        })
-        .catch((error) => {
-          toast.error("Something went wrong");
-          console.log(error);
-          reject(error);
-        });
-    });
+      return response;
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something went wrong");
+      throw error;
+    }
   }
-  delete(url, data) {
-    return new Promise((resolve, reject) => {
-      console.log("this is deleteAPI");
 
-      this.api(METHOD.DELETE, url, data)
-        .then((response) => {
-          toast.success(response?.data?.message);
-          resolve(response);
-        })
-        .catch((error) => {
-          console.log(error);
-          toast.error("Something went wrong");
-          reject(error);
-        });
-    });
+  async delete(url, data) {
+    try {
+      const response = await this.api(METHOD.DELETE, url, data);
+
+      toast.success(response?.data?.message || "Deleted Successfully");
+
+      return response;
+    } catch (error) {
+      toast.error(error?.response?.data?.message || "Something went wrong");
+      throw error;
+    }
   }
-  // Main function with hold the axios request param
+
   api(method, url, data) {
-    return new Promise((resolve, reject) => {
-      console.log('this is mainAPI function');
-      let axiosConfig = {};
-      axiosConfig.method = method;
-      axiosConfig.url = this.baseURL + url;
-      axiosConfig.headers = this.setHeaders(data);
+    let axiosConfig = {
+      method,
+      url: this.baseURL + url,
+      headers: this.setHeaders(data),
+    };
 
-      //  console.log("axiosConfig.headers", axiosConfig.headers);
-      if (data) {
-        if (data) axiosConfig.data = data;
-      }
+    if (data) {
+      axiosConfig.data = data;
+    }
 
-      axios(axiosConfig)
-        .then((response) => {
-          if (
-            response &&
-            response.status === STATUS_CODE.INTERNAL_SERVER_ERROR
-          ) {
-            toast.error("Something went wrong!!");
-          } else {
-            resolve(response);
-            console.log("this is response1")
-            if (response) {
-              // toast.success("success response");
-            }
-            else if (response.status === 200) {
-              toast.success(' response success')
+    return axios(axiosConfig)
+      .then((response) => {
+        if (response.status === STATUS_CODE.INTERNAL_SERVER_ERROR) {
+          toast.error("Something went wrong!");
+        }
 
-            }
-            else {
-              toast.error("Something went wrong");
-            }
-          }
-          // console.log("response", response);
-        })
-        .catch((error) => {
-          console.log("ERROR", error);
-          reject(error);
-        });
-    });
+        return response;
+      })
+      .catch((error) => {
+        console.log("API ERROR:", error);
+        throw error;
+      });
   }
-  // Set the header for request
+
   setHeaders(data) {
     let headers = {};
 
     headers["accept-language"] = "en";
     headers["Accept"] = "application/json";
-    headers["Authorization"] = localStorage.getItem("token");
+
+    const token = localStorage.getItem("token");
+
+    if (token) {
+      headers["Authorization"] = token;
+    }
 
     if (!(data instanceof FormData)) {
       headers["Content-Type"] = "application/json";
@@ -145,4 +107,5 @@ class API {
     return headers;
   }
 }
+
 export default API;
