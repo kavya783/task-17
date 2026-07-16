005ea62f06f3b7293c5f7c260f236716b466e50a
kavya <kavya.actimize@gmail.com>
2026-07-16 16:01:57 +0530

    update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAnalytics } from "firebase/analytics";
 import { getMessaging } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAv084Ntun-ARUYT89NwXkwV4LuBRU4Cq8",
-  authDomain: "hr-portal-7ab0e.firebaseapp.com",
-  projectId: "hr-portal-7ab0e",
-  storageBucket: "hr-portal-7ab0e.firebasestorage.app",
-  messagingSenderId: "194012946842",
-  appId: "1:194012946842:web:5faade00108311cf5dda23",
-  measurementId: "G-BHCMLCTZEQ"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
