28c5910776854a3ad67e024c8c702d4f9f4e1fcc
kavya <kavya.actimize@gmail.com>
2026-07-16 17:57:19 +0530

    add

--- src/firebase.js
+++ src/firebase.js
@@ -11,7 +11,7 @@ const firebaseConfig = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 };
-
+console.log(process.env.REACT_APP_FIREBASE_PROJECT_ID);
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
 
