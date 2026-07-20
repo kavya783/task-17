897aefdcda9d78917981f041885e8d84e8368d6f
kavya <kavya.actimize@gmail.com>
2026-07-20 11:35:14 +0530

    Fix firebase service worker config

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,23 +2,23 @@ importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-app-compat.js
 importScripts("https://www.gstatic.com/firebasejs/10.13.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.REACT_APP_FIREBASE_APP_ID,
-  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyAv084Ntun-ARUYT89NwXkwV4LuBRU4Cq8",
+  authDomain: "hr-portal-7ab0e.firebaseapp.com",
+  projectId: "hr-portal-7ab0e",
+  storageBucket: "hr-portal-7ab0e.firebasestorage.app",
+  messagingSenderId: "194012946842",
+  appId: "1:194012946842:web:5faade00108311cf5dda23",
+  measurementId: "G-BHCMLCTZEQ",
 });
 
 const messaging = firebase.messaging();
 
-// messaging.onBackgroundMessage((payload) => {
-//   self.registration.showNotification(
-//     payload.notification.title,
-//     {DeviceToken.where(user_id: USER_ID)
-//       body: payload.notification.body,
-//       icon: "/hr.png",
-//     }
-//   );
-// });
+messaging.onBackgroundMessage((payload) => {
+  self.registration.showNotification(
+    payload.notification.title,
+    {
+      body: payload.notification.body,
+      icon: "/hr.png",
+    }
+  );
+});
