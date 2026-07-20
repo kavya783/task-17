45e9dd5e5507f3e996b6200c4e6e066b432b078a
kavya <kavya.actimize@gmail.com>
2026-07-20 11:18:20 +0530

    add

--- src/notification.js
+++ src/notification.js
@@ -8,43 +8,35 @@ import { saveDeviceTokenActionInitiate } from "./redux/actions/deviceTokenAction
 
 export const requestNotificationPermission = async (dispatch) => {
   try {
-    // Check browser support
+
     const supported = await isSupported();
 
     if (!supported) {
-      // console.log("Firebase Messaging is not supported in this browser.");
       return;
     }
 
-    // Request notification permission
     const permission = await Notification.requestPermission();
 
+    console.log("Permission:", permission);
+
     if (permission !== "granted") {
-      // console.log("Notification permission denied.");
       return;
     }
 
-    // Get FCM Token
     const token = await getToken(messaging, {
-  vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY,
-});
+      vapidKey: process.env.REACT_APP_FIREBASE_VAPID_KEY,
+    });
 
-    if (!token) {
-      // console.log("FCM Token not generated.");
-      return;
-    }
+    console.log("FCM TOKEN:", token);
 
-    // console.log("FCM Token:", token);
-
-    // Get logged in user id
     const userId = localStorage.getItem("user_id");
 
+    console.log("USER ID:", userId);
+
     if (!userId) {
-      // console.log("User not logged in.");
       return;
     }
 
-    // Save token in backend
     await dispatch(
       saveDeviceTokenActionInitiate({
         user_id: userId,
@@ -52,11 +44,8 @@ export const requestNotificationPermission = async (dispatch) => {
       })
     );
 
-    // console.log("Token saved successfully.");
-
-    return token;
   } catch (error) {
-    // console.error("FCM Error:", error);
+    console.log(error);
   }
 };
 
