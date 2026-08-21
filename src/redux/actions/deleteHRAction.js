11642fccba7e4c3f73024d3cd13098b61ef24fb7
kavya <kavya.actimize@gmail.com>
2026-08-21 18:00:23 +0530

    add

--- src/redux/actions/deleteHRAction.js
+++ src/redux/actions/deleteHRAction.js
@@ -1,46 +1,35 @@
 import * as types from "./actionTypes";
 import { deleteHRData } from "../apis/deleteHRApi";
-import { getHRDataActionInitiate } from "./getHRAction";
-
 
 export const deleteHRDataStart = () => ({
   type: types.DELETE_HR_DATA_START,
 });
-
 
 export const deleteHRDataSuccess = (id) => ({
   type: types.DELETE_HR_DATA_SUCCESS,
   payload: id,
 });
 
-
 export const deleteHRDataError = (error) => ({
   type: types.DELETE_HR_DATA_ERROR,
   payload: error,
 });
 
-
 export const deleteHRDataActionInitiate = (id) => {
-
   return async (dispatch) => {
-
     dispatch(deleteHRDataStart());
 
     try {
-
       await deleteHRData(id);
 
+      // Redux list lo immediate ga remove avutundi
       dispatch(deleteHRDataSuccess(id));
 
-      // refresh list
-      dispatch(getHRDataActionInitiate());
+      return id;
 
     } catch (error) {
-
       dispatch(deleteHRDataError(error.message));
-
+      throw error;
     }
-
   };
-
 };
